a2b9c0533dc49057435a179f0eb4395fd51578ab
Sean Doyle <sdoylelambda@gmail.com>
2019-04-26 12:20:24 -0400

    Smurf.js to show smurf data.

---
+++ smurfs/src/components/Smurf.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Smurf = props => {
+    return (
+        <li>
+            <div className="card">
+            <h2>{props.smurf.name}</h2>
+            </div>
+        </li>
+    );
+};
+
+export default Smurf;
